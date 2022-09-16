a92ca5bcccf5733e079c556f4517d162066a4557
Olesya Polishchuk <poluso1818@gmail.com>
2022-09-16 09:37:11 +0300

    Add sidebarStyles.css file

--- main.js
+++ main.js
@@ -10,26 +10,23 @@
 //     }
 // }
 const sidebarBack = document.querySelector('#sidebar-background-unwrapped');
+const sidebarBackStyles = sidebarBack.style;
+
 const sidebar = document.querySelector('#sidebar-unwrapped');
+const sidebarStyles = sidebar.style;
 
 const burger = document.querySelectorAll('button.burger');
-const sidebarStyles = sidebar.style;
-const sidebarBackStyles = sidebarBack.style;
+const burgerStyles = burger.style;
 
 const burgerOpen = document.querySelector('#burger-open');
 const burgerClose = document.querySelector('#burger-close');
 
-
-
 let isSidebarVisible = true;
-
 // if (sidebarStyles.visibility === 'visible') {
 //   isSidebarVisible = true;
 // } else {
 //   isSidebarVisible = false;
 // }
-
-
 const toggleSidebar = () => {
   if (sidebarBackStyles.visibility === 'visible') {
     sidebarStyles.left = '-240px';
@@ -39,20 +36,30 @@ const toggleSidebar = () => {
     isSidebarVisible = true;
   }
   //console.log("sdaf");
-  // if (sidebarStyles.visibility === )
-
+  // if (sidebarStyles.visibility === ) 
   sidebarBackStyles.visibility = isSidebarVisible ? 'visible' : 'hidden';
   //isSidebarVisible = !isSidebarVisible;
 }
 
-burgerOpen.addEventListener("click", toggleSidebar);
-burgerClose.addEventListener("click", toggleSidebar);
+const changeBack = () => {
+  //burgerOpen.
+  this.classList.add("burger-onclick");
+  //setTimeout(thisElem.classList.remove("burger-onclick"), 200);
+  // burger.forEach(function (burgerIndex) {
+  //   burgerIndex.classList.add("burger-onclick");
+  // })
+  // setTimeout(burger.forEach(function(burgerIndex) {
+  //   burgerIndex.classList.remove("burger-onclick");
+  // }), 2000)
+}
+
+burgerOpen.addEventListener("click", toggleSidebar, changeBack);
+burgerClose.addEventListener("click", toggleSidebar, changeBack);
 
 //const burgerNew = .forEach(burger);
 // for (i of burger) {
 //   i.addEventListener("click", toggleSidebar);
 // }
-
 
 //burger.forEach();
 //console.log(burger);
